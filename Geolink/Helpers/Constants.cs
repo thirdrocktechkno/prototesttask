8cf20a88c0324a1375d33f8521fe603cc0a27261
Tapan Patel <tapan3112@gmail.com>
2019-07-11 21:01:00 +0530

    Update Constants.cs

--- Geolink/Helpers/Constants.cs
+++ Geolink/Helpers/Constants.cs
@@ -3,7 +3,7 @@ namespace Geolink
 {
     public static class Constants
     {
-        public const string GoogleMapsApiKey = "AIzaSyAtrPrn_oyIpHMb_LcrYu-PN9nQ3moVIW8";
+        public const string GoogleMapsApiKey = "";
         public const string LineString = "linestring";
     }
 }
